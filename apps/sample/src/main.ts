3b5283a1c55fcf08b3c0a6cee948fce7a6933b38
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-07 09:35:16 +0700

    Remove unused code

--- apps/sample/src/main.ts
+++ apps/sample/src/main.ts
@@ -4,13 +4,6 @@ import { configRmqService } from '@app/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(SampleModule);
-    // const configService = app.get(ConfigService);
-    // const rmqService = app.get(RabbitMQService);
-
-    // const queue = configService.get('RABBITMQ_SAMPLE_QUEUE');
-
-    // app.connectMicroservice(rmqService.getRmqOptions(queue));
-    // app.startAllMicroservices();
     configRmqService(app, 'RABBITMQ_SAMPLE_QUEUE');
 }
 bootstrap();
