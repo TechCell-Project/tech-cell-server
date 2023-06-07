0476c8d8bf3ee15cea895211cfb201624638ff21
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-07 20:42:22 +0700

    Add ping get request to testing

--- apps/api/src/controllers/app.controller.ts
+++ apps/api/src/controllers/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
-@Controller()
+@Controller('/')
 export class AppController {
     constructor(@Inject('SAMPLE_SERVICE') private readonly sampleService: ClientProxy) {}
+
+    @Get('ping')
+    async getPing() {
+        return { message: 'pong' };
+    }
 
     @Get('sample')
     async getSample() {
