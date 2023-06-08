2296438d77199398477d903e69ee8f6db86471d6
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-08 10:18:03 +0700

    Update main.ts

--- apps/sample/src/main.ts
+++ apps/sample/src/main.ts
@@ -5,5 +5,6 @@ import { configRmqService } from '@app/common';
 async function bootstrap() {
     const app = await NestFactory.create(SampleModule);
     configRmqService(app, 'RABBITMQ_SAMPLE_QUEUE');
+    app.listen(1001);
 }
 bootstrap();
