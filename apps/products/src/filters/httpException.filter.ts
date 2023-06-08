f356fc9e6711de6c0d67680d4cbd76154b88f1b0
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-08 17:46:13 +0700

    Revert "Refactor change port"

--- apps/products/src/filters/httpException.filter.ts
+++
@@ -1,9 +0,0 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
-import { RpcException } from '@nestjs/microservices';
-
-@Catch(HttpException)
-export class RpcValidationFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost) {
-        return new RpcException(exception.getResponse());
-    }
-}
--- libs/common/src/RabbitMQ/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './rmq.module';
-export * from './services';
-export * from './interfaces';
