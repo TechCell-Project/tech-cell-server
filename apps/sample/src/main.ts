efcf61061b744448144220d088381979ed53fcce
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-08 18:06:49 +0700

    Add env port for services

--- apps/sample/src/main.ts
+++ apps/sample/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { SampleModule } from './sample.module';
 import { configRmqService } from '@app/common';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+    const port = process.env.SERVICE_SAMPLE_PORT;
     const app = await NestFactory.create(SampleModule);
     configRmqService(app, 'RABBITMQ_SAMPLE_QUEUE');
-    app.listen(1001);
+    await app.listen(port);
+    Logger.log(`⚡️ [sample] sample listening on http://localhost:${port}`);
 }
 bootstrap();
