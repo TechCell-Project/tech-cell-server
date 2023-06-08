51e008e63497ab35324660e4612b45ab571b436c
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-08 08:33:50 +0700

    Trigger for redeploy

---
+++ dist/apps/sample/main.js
@@ -0,0 +1,483 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("@nestjs/core");
+
+/***/ }),
+/* 2 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.SampleModule = void 0;
+const common_1 = __webpack_require__(3);
+const sample_controller_1 = __webpack_require__(4);
+const sample_service_1 = __webpack_require__(5);
+const common_2 = __webpack_require__(6);
+let SampleModule = exports.SampleModule = class SampleModule {
+};
+exports.SampleModule = SampleModule = __decorate([
+    (0, common_1.Module)({
+        imports: [common_2.RabbitMQModule],
+        controllers: [sample_controller_1.SampleController],
+        providers: [common_2.RabbitMQService, sample_service_1.SampleService],
+    })
+], SampleModule);
+
+
+/***/ }),
+/* 3 */
+/***/ ((module) => {
+
+module.exports = require("@nestjs/common");
+
+/***/ }),
+/* 4 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+var _a, _b, _c;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.SampleController = void 0;
+const common_1 = __webpack_require__(3);
+const sample_service_1 = __webpack_require__(5);
+const common_2 = __webpack_require__(6);
+const microservices_1 = __webpack_require__(10);
+let SampleController = exports.SampleController = class SampleController {
+    constructor(sampleService, rabbitMqService) {
+        this.sampleService = sampleService;
+        this.rabbitMqService = rabbitMqService;
+    }
+    getHello() {
+        return this.sampleService.getHello();
+    }
+    async getSample(context) {
+        this.rabbitMqService.acknowledgeMessage(context);
+        return { message: 'hello from sample' };
+    }
+};
+__decorate([
+    (0, common_1.Get)(),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", String)
+], SampleController.prototype, "getHello", null);
+__decorate([
+    (0, microservices_1.MessagePattern)('get_sample'),
+    __param(0, (0, microservices_1.Ctx)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [typeof (_c = typeof microservices_1.RmqContext !== "undefined" && microservices_1.RmqContext) === "function" ? _c : Object]),
+    __metadata("design:returntype", Promise)
+], SampleController.prototype, "getSample", null);
+exports.SampleController = SampleController = __decorate([
+    (0, common_1.Controller)(),
+    __param(1, (0, common_1.Inject)(common_2.RabbitMQService)),
+    __metadata("design:paramtypes", [typeof (_a = typeof sample_service_1.SampleService !== "undefined" && sample_service_1.SampleService) === "function" ? _a : Object, typeof (_b = typeof common_2.RabbitMQService !== "undefined" && common_2.RabbitMQService) === "function" ? _b : Object])
+], SampleController);
+
+
+/***/ }),
+/* 5 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.SampleService = void 0;
+const common_1 = __webpack_require__(3);
+let SampleService = exports.SampleService = class SampleService {
+    getHello() {
+        return 'Hello World!';
+    }
+};
+exports.SampleService = SampleService = __decorate([
+    (0, common_1.Injectable)()
+], SampleService);
+
+
+/***/ }),
+/* 6 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__exportStar(__webpack_require__(7), exports);
+__exportStar(__webpack_require__(16), exports);
+
+
+/***/ }),
+/* 7 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__exportStar(__webpack_require__(8), exports);
+__exportStar(__webpack_require__(11), exports);
+__exportStar(__webpack_require__(14), exports);
+
+
+/***/ }),
+/* 8 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var RabbitMQModule_1;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.RabbitMQModule = void 0;
+const common_1 = __webpack_require__(3);
+const config_1 = __webpack_require__(9);
+const microservices_1 = __webpack_require__(10);
+const services_1 = __webpack_require__(11);
+let RabbitMQModule = exports.RabbitMQModule = RabbitMQModule_1 = class RabbitMQModule {
+    static registerRmq(service, queue) {
+        const providers = [
+            {
+                provide: service,
+                useFactory: (configService) => {
+                    const URLS = configService.get('RABBITMQ_URLS');
+                    return microservices_1.ClientProxyFactory.create({
+                        transport: microservices_1.Transport.RMQ,
+                        options: {
+                            urls: [URLS],
+                            queue,
+                            queueOptions: {
+                                durable: true,
+                            },
+                        },
+                    });
+                },
+                inject: [config_1.ConfigService],
+            },
+        ];
+        return {
+            module: RabbitMQModule_1,
+            providers,
+            exports: providers,
+        };
+    }
+};
+exports.RabbitMQModule = RabbitMQModule = RabbitMQModule_1 = __decorate([
+    (0, common_1.Module)({
+        imports: [
+            config_1.ConfigModule.forRoot({
+                isGlobal: true,
+                envFilePath: './.env',
+            }),
+        ],
+        providers: [services_1.RabbitMQService],
+        exports: [services_1.RabbitMQService],
+    })
+], RabbitMQModule);
+
+
+/***/ }),
+/* 9 */
+/***/ ((module) => {
+
+module.exports = require("@nestjs/config");
+
+/***/ }),
+/* 10 */
+/***/ ((module) => {
+
+module.exports = require("@nestjs/microservices");
+
+/***/ }),
+/* 11 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__exportStar(__webpack_require__(12), exports);
+__exportStar(__webpack_require__(13), exports);
+
+
+/***/ }),
+/* 12 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var _a;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.RabbitMQService = void 0;
+const common_1 = __webpack_require__(3);
+const config_1 = __webpack_require__(9);
+const microservices_1 = __webpack_require__(10);
+let RabbitMQService = exports.RabbitMQService = class RabbitMQService {
+    constructor(configService) {
+        this.configService = configService;
+    }
+    getRmqOptions(queue, noAck = false) {
+        const URLS = this.configService.get('RABBITMQ_URLS');
+        return {
+            transport: microservices_1.Transport.RMQ,
+            options: {
+                urls: [URLS],
+                noAck,
+                queue,
+                queueOptions: {
+                    durable: true,
+                },
+            },
+        };
+    }
+    acknowledgeMessage(context) {
+        const channel = context.getChannelRef();
+        const message = context.getMessage();
+        channel.ack(message);
+    }
+};
+exports.RabbitMQService = RabbitMQService = __decorate([
+    (0, common_1.Injectable)(),
+    __metadata("design:paramtypes", [typeof (_a = typeof config_1.ConfigService !== "undefined" && config_1.ConfigService) === "function" ? _a : Object])
+], RabbitMQService);
+
+
+/***/ }),
+/* 13 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.configRmqService = void 0;
+const common_1 = __webpack_require__(3);
+const config_1 = __webpack_require__(9);
+const common_2 = __webpack_require__(6);
+async function configRmqService(app, queueNameEnv) {
+    const configService = app.get(config_1.ConfigService);
+    const rmqService = app.get(common_2.RabbitMQService);
+    const queue = configService.get(queueNameEnv);
+    app.connectMicroservice(rmqService.getRmqOptions(queue));
+    await app.startAllMicroservices();
+    common_1.Logger.log(`⚡️ [${queue}] Started listening on queue: ${queue}`);
+}
+exports.configRmqService = configRmqService;
+
+
+/***/ }),
+/* 14 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__exportStar(__webpack_require__(15), exports);
+
+
+/***/ }),
+/* 15 */
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+
+
+/***/ }),
+/* 16 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__exportStar(__webpack_require__(17), exports);
+
+
+/***/ }),
+/* 17 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.MongodbModule = void 0;
+const common_1 = __webpack_require__(3);
+const config_1 = __webpack_require__(9);
+const mongoose_1 = __webpack_require__(18);
+let MongodbModule = exports.MongodbModule = class MongodbModule {
+};
+exports.MongodbModule = MongodbModule = __decorate([
+    (0, common_1.Module)({
+        imports: [
+            config_1.ConfigModule.forRoot({
+                isGlobal: true,
+                envFilePath: './.env',
+            }),
+            mongoose_1.MongooseModule.forRootAsync({
+                useFactory: (configService) => ({
+                    uri: configService.get('MONGODB_URI'),
+                }),
+                inject: [config_1.ConfigService],
+            }),
+        ],
+    })
+], MongodbModule);
+
+
+/***/ }),
+/* 18 */
+/***/ ((module) => {
+
+module.exports = require("@nestjs/mongoose");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core_1 = __webpack_require__(1);
+const sample_module_1 = __webpack_require__(2);
+const common_1 = __webpack_require__(6);
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(sample_module_1.SampleModule);
+    (0, common_1.configRmqService)(app, 'RABBITMQ_SAMPLE_QUEUE');
+}
+bootstrap();
+
+})();
+
+/******/ })()
+;
