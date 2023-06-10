b08b4d838f3d490a1ad560f9364cad873973e50b
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-10 17:06:33 +0700

    Filter for response error

--- libs/common/src/filters/httpException.filter.ts
+++ libs/common/src/filters/httpException.filter.ts
@@ -1,7 +1,8 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 import { RpcException } from '@nestjs/microservices';
 
-@Catch(HttpException)
+@Catch(HttpException, ExceptionsHandler)
 export class RpcValidationFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
         return new RpcException(exception.getResponse());
