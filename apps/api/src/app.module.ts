96c0c296838c38cad0d99b7a6e4aeae4fe6d8281
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-16 23:21:35 +0700

    Add throttle to rate limit

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { RabbitMQModule } from '@app/common';
 import Controller from './controllers';
 import { PRODUCTS_SERVICE, SAMPLE_SERVICE, AUTH_SERVICE } from '~/constants';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
     imports: [
@@ -10,10 +12,20 @@ import { PRODUCTS_SERVICE, SAMPLE_SERVICE, AUTH_SERVICE } from '~/constants';
             isGlobal: true,
             envFilePath: './.env',
         }),
+        ThrottlerModule.forRoot({
+            ttl: 60 * 5, // 5 minutes
+            limit: 500, // request per ttl
+        }),
         RabbitMQModule.registerRmq(SAMPLE_SERVICE, process.env.RABBITMQ_SAMPLE_QUEUE),
         RabbitMQModule.registerRmq(PRODUCTS_SERVICE, process.env.RABBITMQ_PRODUCTS_QUEUE),
         RabbitMQModule.registerRmq(AUTH_SERVICE, process.env.RABBITMQ_AUTH_QUEUE),
     ],
     controllers: [...Controller],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+    ],
 })
 export class AppModule {}
