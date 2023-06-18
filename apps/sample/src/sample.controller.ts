d47afdc758a6111542c3ea65a0334b5115c4d7af
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-18 21:56:19 +0700

    Create a sample for using cache

--- apps/sample/src/sample.controller.ts
+++ apps/sample/src/sample.controller.ts
@@ -1,24 +1,28 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Post } from '@nestjs/common';
 import { SampleService } from './sample.service';
 import { RabbitMQService } from '@app/common';
 import { Ctx, RmqContext, MessagePattern } from '@nestjs/microservices';
-import { Cache } from 'cache-manager';
-import { REDIS_CACHE } from '~/constants';
 
 @Controller()
 export class SampleController {
     constructor(
         private readonly sampleService: SampleService,
         @Inject(RabbitMQService) private readonly rabbitMqService: RabbitMQService,
-        @Inject(REDIS_CACHE) private cacheManager: Cache,
     ) {}
 
-    private count = 0;
     @Get('ping')
     async getPing() {
-        this.count++;
-        await this.cacheManager.set(`key ${this.count}`, `value ${this.count}`);
         return this.sampleService.getPing();
+    }
+
+    @Post('cache')
+    async setCache() {
+        return this.sampleService.setCacheNumber();
+    }
+
+    @Get('cache')
+    async getCache() {
+        return this.sampleService.getCacheNumber();
     }
 
     @MessagePattern('get_sample')
