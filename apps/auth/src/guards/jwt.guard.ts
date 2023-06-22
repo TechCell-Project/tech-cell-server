41e3c8bbf9eaa5f778fef8faca1dcc5ab1ec14e4
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-22 13:09:11 +0700

    Revert "catch the unauthorize token"
    This reverts commit 4f81291ab3cd47916b926ca4e98e9942ec3029b7.

--- apps/auth/src/guards/jwt.guard.ts
+++ apps/auth/src/guards/jwt.guard.ts
@@ -1,13 +1,5 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { RpcException } from '@nestjs/microservices';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtGuard extends AuthGuard('jwt') {
-    handleRequest(err, user, info, context, status) {
-        if (err || !user) {
-            throw new RpcException(new UnauthorizedException(err || 'User not authorized'));
-        }
-        return { ...user, info, context, status };
-    }
-}
+export class JwtGuard extends AuthGuard('jwt') {}
