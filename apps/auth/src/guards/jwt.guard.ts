595e2e7e6cf55aafe97dec6891c20bbc753d9ba2
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-22 13:52:55 +0700

    catch the unauthorize token

--- apps/auth/src/guards/jwt.guard.ts
+++ apps/auth/src/guards/jwt.guard.ts
@@ -1,5 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtGuard extends AuthGuard('jwt') {}
+export class JwtGuard extends AuthGuard('jwt') {
+    handleRequest(err, user, info, context, status) {
+        if (err || !user) {
+            throw new RpcException(new UnauthorizedException(err || 'User not authorized'));
+        }
+        return { ...user, info, context, status };
+    }
+}
