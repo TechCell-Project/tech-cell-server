a5ce23931de088af31439ad83077d0370e9d16cd
Lê Huy Giang <lehuygiang28@gmail.com>
2023-06-28 18:10:21 +0700

    the true type is string

--- apps/auth/src/interfaces/token-verified-response.interface.ts
+++ apps/auth/src/interfaces/token-verified-response.interface.ts
@@ -3,7 +3,7 @@ import { UserRole } from '@app/resource/users/enums';
 export interface ITokenVerifiedResponse {
     readonly _id: string;
     readonly email: string;
-    readonly role: UserRole;
+    readonly role: UserRole | string;
     readonly exp: number;
     readonly iat: number;
 }
