493fc52981bba9414ca6728e48f1f354303a53bc
Lê Huy Giang <lehuygiang28@gmail.com>
2023-07-07 21:25:01 +0700

    rename commnue to ward

--- libs/resource/src/users/dtos/address.schema.dto.ts
+++ libs/resource/src/users/dtos/address.schema.dto.ts
@@ -7,8 +7,8 @@ export class AddressSchemaDTO {
     @ApiProperty({ description: 'The district level address', example: 'Hoang Mai' })
     districtLevel: string;
 
-    @ApiProperty({ description: 'The commune level address', example: 'Mai Dong' })
-    communeLevel: string;
+    @ApiProperty({ description: 'The ward level address', example: 'Mai Dong' })
+    wardLevel: string;
 
     @ApiProperty({ description: 'The detailed address', example: '18 Tam Trinh' })
     detail?: string;
