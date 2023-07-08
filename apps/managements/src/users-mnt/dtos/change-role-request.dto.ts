0251666c4670c1edae6560f8a02ba3e667e1fec3
Lê Huy Giang <lehuygiang28@gmail.com>
2023-07-08 12:34:10 +0700

    update(swagger): add docs for `change-role`

--- apps/managements/src/users-mnt/dtos/change-role-request.dto.ts
+++ apps/managements/src/users-mnt/dtos/change-role-request.dto.ts
@@ -1,11 +1,22 @@
 import { UserRole } from '@app/resource/users/enums';
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 export class ChangeRoleRequestDTO {
+    /**
+     * userId is auto passed from middleware
+     */
+    @ApiHideProperty()
     @IsNotEmpty()
     @IsString()
     userId: string;
 
+    @ApiProperty({
+        description: 'User role',
+        example: 'Admin',
+        type: 'string',
+        enum: UserRole,
+    })
     @IsNotEmpty()
     @IsString()
     @IsEnum(UserRole)
