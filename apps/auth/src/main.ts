be5d5f3c4a3839688b9b080014f04897366aa47a
Lê Huy Giang <lehuygiang28@gmail.com>
2023-07-15 00:21:40 +0700

    refactor(logger): refactor logger

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -6,11 +6,12 @@ import { RpcExceptionFilter } from '@app/common/filters/';
 
 async function bootstrap() {
     const port = process.env.SERVICE_AUTH_PORT;
+    const logger = new Logger('auth');
     const app = await NestFactory.create(AuthModule);
     app.useGlobalFilters(new RpcExceptionFilter());
     useRabbitMQ(app, 'RABBITMQ_AUTH_QUEUE');
     await app.startAllMicroservices();
     await app.listen(port);
-    Logger.log(`⚡️ [Server] Listening on http://localhost:${port}`);
+    logger.log(`⚡️ ready on port: ${port}, url: http://localhost:${port}`);
 }
 bootstrap();
