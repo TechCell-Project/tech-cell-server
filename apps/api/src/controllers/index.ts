3b9029d070c5ffa3291b668392960c4531224b2e
Lê Huy Giang <lehuygiang28@gmail.com>
2023-07-14 11:20:49 +0700

    refactor(api): remove `/mnt/*`, use their name instead (`users, products...`)

--- apps/api/src/controllers/index.ts
+++ apps/api/src/controllers/index.ts
@@ -1,17 +1,13 @@
-export * from './app.controller';
-export * from './products.controller';
-export * from './app.controller';
-
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
 import { AuthController } from './auth.controller';
-import { ManagementsController } from './managements.controller';
 import { OrderController } from './order.controller';
+import { UsersController } from './users.controller';
 
 export default [
-    AppController,
-    ProductsController,
     AuthController,
-    ManagementsController,
+    UsersController,
+    ProductsController,
     OrderController,
+    AppController,
 ];
