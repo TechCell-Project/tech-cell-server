3c42ea98dd5f78ace1fe23b090a4db50ec25f088
Lê Huy Giang <lehuygiang28@gmail.com>
2023-08-27 17:10:49 +0700

    update(swagger): update docs for `cart` endpoint

--- apps/api/src/controllers/carts.controller.ts
+++ apps/api/src/controllers/carts.controller.ts
@@ -2,9 +2,9 @@ import { AuthGuard, catchException } from '@app/common';
 import { CurrentUser } from '@app/common/decorators';
 import { PaginationQuery } from '@app/common/dtos';
 import { ICurrentUser } from '@app/common/interfaces';
-import { Body, Controller, Get, Inject, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Inject, Post, Query, UseGuards } from '@nestjs/common';
 import { ClientRMQ } from '@nestjs/microservices';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CartsMntMessagePattern } from '~/apps/managements/carts-mnt';
 import { AddCartRequestDTO } from '~/apps/managements/carts-mnt/dtos/create-cart-request.dto';
 import { CartsSearchMessagePattern } from '~/apps/search/carts-search/carts-search.pattern';
@@ -26,6 +26,8 @@ export class CartsController {
             .pipe(catchException());
     }
 
+    @ApiOkResponse({ description: 'Cart updated!' })
+    @HttpCode(200)
     @Post('/')
     async addCart(@Body() { ...cartData }: AddCartRequestDTO, @CurrentUser() user: ICurrentUser) {
         return this.managementsService
