8c6d9f2f3da39a27b5e19b9dca82b7f7afa8a7b2
Lê Huy Giang <lehuygiang28@gmail.com>
2023-08-31 18:03:43 +0700

    refactor(main): remove unused port in each service, use microService instead

--- apps/sample/src/main.ts
+++ apps/sample/src/main.ts
@@ -5,13 +5,11 @@ import { Logger } from '@nestjs/common';
 import { RpcExceptionFilter } from '@app/common/filters/';
 
 async function bootstrap() {
-    const port = process.env.SERVICE_SAMPLE_PORT;
     const logger = new Logger('sample');
     const app = await NestFactory.create(SampleModule);
     app.useGlobalFilters(new RpcExceptionFilter());
     useRabbitMQ(app, 'RABBITMQ_SAMPLE_QUEUE');
     await app.startAllMicroservices();
-    await app.listen(port);
-    logger.log(`⚡️ ready on port: ${port}, url: http://localhost:${port}`);
+    logger.log(`⚡️ service is ready`);
 }
 bootstrap();
