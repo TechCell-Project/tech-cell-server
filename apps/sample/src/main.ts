6cddcba8e4a51780642244513eef78ffc40b53f2
Lê Huy Giang <lehuygiang28@gmail.com>
2023-09-07 00:39:25 +0700

    feat(sample): auto change port if port already in use

--- apps/sample/src/main.ts
+++ apps/sample/src/main.ts
@@ -3,13 +3,34 @@ import { SampleModule } from './sample.module';
 import { useRabbitMQ } from '@app/common';
 import { Logger } from '@nestjs/common';
 import { RpcExceptionFilter } from '@app/common/filters/';
+import * as net from 'net';
 
 async function bootstrap() {
     const logger = new Logger('sample');
     const app = await NestFactory.create(SampleModule);
     app.useGlobalFilters(new RpcExceptionFilter());
     useRabbitMQ(app, 'RABBITMQ_SAMPLE_QUEUE');
-    await app.startAllMicroservices().then(() => logger.log(`⚡️ service is ready`));
-    app.listen(3000, () => logger.log(`⚡️ server is listening port ...`));
+
+    const port: string = process.env.SAMPLE_PORT || '3000';
+    const server = net.createServer().listen(port);
+
+    server.on('listening', () => {
+        server.close();
+        app.startAllMicroservices().then(() => logger.log(`⚡️ service is ready`));
+        app.listen(port, () => logger.log(`⚡️ server is listening on port ${port}`));
+    });
+
+    server.on('error', (err: any) => {
+        if (err.code === 'EADDRINUSE') {
+            logger.error(`Port ${port} is already in use. Trying another port...`);
+            server.close();
+            const newPort = parseInt(port, 10) + 1;
+            app.startAllMicroservices().then(() => logger.log(`⚡️ service is ready`));
+            app.listen(newPort, () => logger.log(`⚡️ server is listening on port ${newPort}`));
+        } else {
+            logger.error(err);
+        }
+    });
 }
+
 bootstrap();
