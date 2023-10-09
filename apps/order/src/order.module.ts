a4268a7a08ecf3c76b0f23a0c6bb3f9f7728ddd1
Lê Huy Giang <lehuygiang28@gmail.com>
2023-10-09 15:47:43 +0700

    refactor(lib): move vnpay to `third-party` libs

--- apps/order/src/order.module.ts
+++ apps/order/src/order.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule, RabbitMQService } from '@app/common';
-import { PaymentModule } from '@app/payment';
 import { CartsOrdModule } from '~/apps/order/carts-ord';
 import { GhtkModule } from '@app/third-party/giaohangtietkiem.vn';
+import { VnpayModule } from '@app/third-party/vnpay';
 import { CheckoutModule } from './checkout-ord/checkout.module';
 
 @Module({
-    imports: [AppConfigModule, PaymentModule, CartsOrdModule, GhtkModule, CheckoutModule],
+    imports: [AppConfigModule, VnpayModule, CartsOrdModule, GhtkModule, CheckoutModule],
     providers: [RabbitMQService],
 })
 export class OrderModule {}
