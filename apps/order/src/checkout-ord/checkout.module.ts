6f7b4a15b110cdc6aeb6bcee802d346615e6f89a
Lê Huy Giang <lehuygiang28@gmail.com>
2023-10-11 14:27:15 +0700

    feat(payment): add vnpay

--- apps/order/src/checkout-ord/checkout.module.ts
+++ apps/order/src/checkout-ord/checkout.module.ts
@@ -3,11 +3,21 @@ import { RabbitMQService } from '@app/common';
 import { HttpModule } from '@nestjs/axios';
 import { CheckoutService } from './checkout.service';
 import { CheckoutController } from './checkout.controller';
-import { GhnModule } from '@app/third-party/giaohangnhanh';
 import { UsersModule } from '@app/resource';
+import { GhnModule } from '@app/third-party/giaohangnhanh';
+import { VnpayModule } from '@app/third-party/vnpay.vn';
 
 @Module({
-    imports: [HttpModule, GhnModule, UsersModule],
+    imports: [
+        HttpModule,
+        GhnModule,
+        UsersModule,
+        VnpayModule.forRoot({
+            paymentGateway: process.env.VNPAY_PAYMENT_URL,
+            secureSecret: process.env.VNPAY_SECRET_KEY,
+            tmnCode: process.env.VNPAY_TMN_CODE,
+        }),
+    ],
     controllers: [CheckoutController],
     providers: [RabbitMQService, CheckoutService],
 })
--- apps/order/src/order.module.ts
+++ apps/order/src/order.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule, RabbitMQService } from '@app/common';
 import { CartsOrdModule } from '~/apps/order/carts-ord';
-import { VnpayModule } from '@app/third-party/vnpay.vn';
 import { CheckoutModule } from './checkout-ord/checkout.module';
 
 @Module({
-    imports: [AppConfigModule, VnpayModule, CartsOrdModule, CheckoutModule],
+    imports: [AppConfigModule, CartsOrdModule, CheckoutModule],
     providers: [RabbitMQService],
 })
 export class OrderModule {}
