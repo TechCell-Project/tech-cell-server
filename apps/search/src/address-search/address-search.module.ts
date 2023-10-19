fcd32cb73fda8749ad6b617726df4625fc674c11
Lê Huy Giang <lehuygiang28@gmail.com>
2023-10-19 23:22:07 +0700

    feat(address): optimize search address with cache (#209)
    * feat(address): optimize search address with cache
    
    * fix(): remove unused

--- apps/search/src/address-search/address-search.module.ts
+++ apps/search/src/address-search/address-search.module.ts
@@ -2,9 +2,10 @@ import { GhnModule } from '@app/third-party';
 import { Module } from '@nestjs/common';
 import { AddressSearchController } from './address-search.controller';
 import { AddressSearchService } from './address-search.service';
+import { RedisCacheModule } from '@app/common';
 
 @Module({
-    imports: [GhnModule],
+    imports: [GhnModule, RedisCacheModule],
     controllers: [AddressSearchController],
     providers: [AddressSearchService],
 })
---
+++ libs/third-party/src/giaohangnhanh/dtos/index.ts
@@ -0,0 +1,4 @@
+export * from './district.dto';
+export * from './province.dto';
+export * from './ward.dto';
+export * from './get-shipping-fee.dto';
