efba56d2a9b34fbffc04ae3cd32e0eb628582da7
Lê Huy Giang <lehuygiang28@gmail.com>
2023-10-24 22:21:13 +0700

    update(ioredis): move to use `ioredis` instead of `redis`

--- libs/common/src/RedisCache/redisCache.module.ts
+++ libs/common/src/RedisCache/redisCache.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { redisStore } from 'cache-manager-redis-yet';
+import { redisStore } from 'cache-manager-ioredis-yet';
 import { REDIS_CACHE } from '@app/common/constants';
-import { RedisService } from './services/redis.service';
 
 @Module({
     providers: [
@@ -9,16 +8,13 @@ import { RedisService } from './services/redis.service';
             provide: REDIS_CACHE,
             useFactory: async () =>
                 await redisStore({
-                    socket: {
-                        host: process.env.REDIS_HOST,
-                        port: +process.env.REDIS_PORT, // '+' means convert string to number
-                    },
+                    host: process.env.REDIS_HOST,
+                    port: +process.env.REDIS_PORT, // '+' means convert string to number
                     password: process.env.REDIS_PASSWORD,
                     ttl: 5000, // 5 secs
                 }),
         },
-        RedisService,
     ],
-    exports: [REDIS_CACHE, RedisService],
+    exports: [REDIS_CACHE],
 })
 export class RedisCacheModule {}
