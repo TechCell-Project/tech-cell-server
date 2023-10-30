6e2b7555ba0abc6df4869513bade7611ff9b1e65
Lê Huy Giang <lehuygiang28@gmail.com>
2023-10-30 15:52:12 +0700

    update(domain): update `ws` subdomain for `socket.io` (#231)
    * update(domain): update `ws` subdomain for `socket.io`
    
    * feat(communications): remove unused

--- apps/communications/src/communications.controller.ts
+++ apps/communications/src/communications.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Res, Get } from '@nestjs/common';
 import { Response } from 'express';
+import { Controller, Res, Get } from '@nestjs/common';
+import { NotificationsMessageSubscribe } from './notifications/constants/notifications.message';
 
 @Controller('/')
 export class CommunicationsController {
     @Get('/')
-    getIndex(@Res() res: Response) {
-        return res.redirect('https://api.techcell.cloud');
+    @Get('/ping')
+    async ping(@Res() res: Response) {
+        res.json({
+            message: 'pong',
+            availableRoutes: ['/ping', '/notifications'],
+        });
+    }
+
+    @Get('/notifications')
+    async getNotifications(@Res() res: Response) {
+        res.json({
+            message: 'Notifications endpoint',
+            allMessage: Object.values(NotificationsMessageSubscribe).join(', '),
+        });
     }
 }
