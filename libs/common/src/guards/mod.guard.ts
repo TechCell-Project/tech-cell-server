59b178b348442a51b8c2ca602697295e1c22a914
lehuygiang28 <lehuygiang28@gmail.com>
2023-11-16 12:14:21 +0700

    Add CanActivate interface to guard classes

--- libs/common/src/guards/mod.guard.ts
+++ libs/common/src/guards/mod.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { CanActivate, Injectable } from '@nestjs/common';
 import { UserRole } from '@app/resource/users/enums';
 import { AuthCoreGuard } from './auth.core.guard';
 import { Reflector } from '@nestjs/core';
@@ -7,7 +7,7 @@ import { Reflector } from '@nestjs/core';
  * @description ModGuard: required login for mod or higher roles
  */
 @Injectable()
-export class ModGuard extends AuthCoreGuard {
+export class ModGuard extends AuthCoreGuard implements CanActivate  {
     constructor() {
         super(new Reflector(), ModGuard.name);
         this._acceptRoles.push(UserRole.SuperAdmin, UserRole.Admin, UserRole.Mod);
