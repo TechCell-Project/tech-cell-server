1faa71ba048706a38bc5db40738775dce82999fb
lehuygiang28 <lehuygiang28@gmail.com>
2023-11-17 16:00:32 +0700

    Fix imports in products.controller.ts

--- apps/managements/src/products-mnt/dtos/index.ts
+++ apps/managements/src/products-mnt/dtos/index.ts
@@ -1,4 +1,3 @@
 export * from './create-product-request.dto';
-// export * from './change-status.dto';
-// export * from './change-status-request.dto';
-// export * from './update-product-request.dto';
+export * from './update-product-request.dto';
+export * from './params.dto';
