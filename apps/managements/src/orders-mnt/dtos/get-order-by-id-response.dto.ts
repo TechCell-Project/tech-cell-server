711999c8155ce4ebe40eeca9e04885d991e3b014
lehuygiang28 <lehuygiang28@gmail.com>
2023-11-17 17:43:46 +0700

    Update GetOrderByIdResponseDTO to include generalImages in ProductsOrder

--- apps/managements/src/orders-mnt/dtos/get-order-by-id-response.dto.ts
+++ apps/managements/src/orders-mnt/dtos/get-order-by-id-response.dto.ts
@@ -1,8 +1,12 @@
-import { ApiProperty, IntersectionType, OmitType } from '@nestjs/swagger';
+import { ApiProperty, IntersectionType, OmitType, PickType } from '@nestjs/swagger';
 import { OrderSchemaDTO, ProductOrderSchemaDTO } from '~libs/resource/orders/dtos/order-schema.dto';
-import { ProductVariationDTO } from '~libs/resource/products/dtos/product.dto';
+import { ProductVariationDTO, ProductDTO } from '~libs/resource/products/dtos/product.dto';
 
-class ProductsOrder extends IntersectionType(ProductOrderSchemaDTO, ProductVariationDTO) {}
+class ProductsOrder extends IntersectionType(
+    ProductOrderSchemaDTO,
+    ProductVariationDTO,
+    PickType(ProductDTO, ['generalImages']),
+) {}
 
 export class GetOrderByIdResponseDTO extends OmitType(OrderSchemaDTO, ['products']) {
     @ApiProperty({ type: [ProductsOrder] })
