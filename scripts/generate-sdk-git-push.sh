70ab8b1c0fd8a5bd1228c77edd6c436c38150a3f
lehuygiang28 <lehuygiang28@gmail.com>
2023-12-15 04:19:45 +0700

    Update CI/CD workflow and generate SDKs [skip docker]

--- scripts/generate-sdk-git-push.sh
+++ scripts/generate-sdk-git-push.sh
@@ -1,57 +1,57 @@
 #!/bin/sh
 # ref: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
 #
-# Usage example: /bin/sh ./generate-sdk-git-push.sh "lehuygiang28" "tech-cell-server-sdk" "minor update" main
+# Usage example: /bin/sh ./git_push.sh wing328 openapi-petstore-perl "minor update" "gitlab.com"
 
-git_user_id=${1:-"GIT_USER_ID"}
-git_repo_id=${2:-"GIT_REPO_ID"}
-release_note=${3:-"Minor update"}
-branch=${4:-"master"}
-script_generate_sdk=${5:-""}
-directory_generated_sdk=${6:-""}
+git_user_id=$1
+git_repo_id=$2
+release_note=$3
+git_host=$4
 
-if [ ! -d "$directory_generated_sdk" ]; then
-    echo "Error: $directory_generated_sdk does not exist"
-    exit 1
+if [ "$git_host" = "" ]; then
+    git_host="github.com"
+    echo "[INFO] No command line input provided. Set \$git_host to $git_host"
 fi
 
-# Change to the directory
-cd $directory_generated_sdk || { echo "Error: Unable to change to the directory $directory_generated_sdk"; exit 1; }
+if [ "$git_user_id" = "" ]; then
+    git_user_id="GIT_USER_ID"
+    echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
+fi
+
+if [ "$git_repo_id" = "" ]; then
+    git_repo_id="GIT_REPO_ID"
+    echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
+fi
+
+if [ "$release_note" = "" ]; then
+    release_note="Minor update"
+    echo "[INFO] No command line input provided. Set \$release_note to $release_note"
+fi
 
 # Initialize the local directory as a Git repository
-if [ ! -d ".git" ]; then
-    git init || { echo "Error: Unable to initialize the Git repository"; exit 1; }
-fi
+git init
 
-# Sets the new remote if not defined
-if [ -z "$(git remote)" ]; then
-    if [ -z "$GIT_TOKEN" ]; then
+# Adds the files in the local repository and stages them for commit.
+git add .
+
+# Commits the tracked changes and prepares them to be pushed to a remote repository.
+git commit -m "$release_note"
+
+# Sets the new remote
+git_remote=$(git remote)
+if [ "$git_remote" = "" ]; then # git remote not defined
+
+    if [ "$GIT_TOKEN" = "" ]; then
         echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git credential in your environment."
-        git remote add origin "https://github.com/${git_user_id}/${git_repo_id}.git" || { echo "Error: Unable to add remote"; exit 1; }
+        git remote add origin https://${git_host}/${git_user_id}/${git_repo_id}.git
     else
-        git remote add origin "https://${git_user_id}:${GIT_TOKEN}@github.com/${git_user_id}/${git_repo_id}.git" || { echo "Error: Unable to add remote"; exit 1; }
+        git remote add origin https://${git_user_id}:"${GIT_TOKEN}"@${git_host}/${git_user_id}/${git_repo_id}.git
     fi
+
 fi
 
-git pull
+git pull origin master
 
-git checkout $branch || { echo "Error: Unable to switch to branch $branch"; exit 1; }
-
-rm -rf ./* ./.??* || { echo "Error: Unable to remove files in the current directory"; exit 1; }
-
-if [ -z "$script_generate_sdk" ]; then
-    echo "Error: script_generate_sdk is not defined"
-    exit 1
-fi
-
-$script_generate_sdk || { echo "Error: Unable to generate SDK"; exit 1; }
-
-# Add the generated files
-git add . || { echo "Error: Unable to add files to the staging area"; exit 1; }
-
-# Commit the changes
-git commit -a -m "$release_note" || { echo "Error: Unable to commit changes"; exit 1; }
-
-# Push the changes
-echo "Git pushing to https://github.com/${git_user_id}/${git_repo_id}.git"
-git push origin $branch || { echo "Error: Unable to push changes to the remote repository"; exit 1; }
+# Pushes (Forces) the changes in the local repository up to the remote repository
+echo "Git pushing to https://${git_host}/${git_user_id}/${git_repo_id}.git"
+git push --force-with-lease origin master 2>&1 | grep -v 'To https'
