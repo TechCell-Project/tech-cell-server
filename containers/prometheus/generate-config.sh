2b85ddb934f5bfdb721d1b424bbeddaf6d2f8e58
lehuygiang28 <lehuygiang28@gmail.com>
2023-12-27 03:12:25 +0700

    feat(metrics): :rocket: create prometheus

---
+++ containers/prometheus/generate-config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cat << EOF > /etc/prometheus/prometheus.yml
+scrape_configs:
+  - job_name: "sws-authtest"
+    scrape_interval: "10s"
+    metrics_path: "/api-stats/metrics"
+    static_configs:
+      - targets: [localhost:3050]
+    basic_auth:
+      username: "${STATS_USER}"
+      password: "${STATS_PASSWORD}"
+EOF
