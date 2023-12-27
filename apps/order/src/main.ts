fcb26fd938188870e0a8fbcc9788e3262deaee2d
lehuygiang28 <lehuygiang28@gmail.com>
2023-12-28 02:19:31 +0700

    update(checkout): :zap: add global HTTP to RPC exception filter

--- apps/order/src/main.ts
+++ apps/order/src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { OrderModule } from './order.module';
 import { RabbitMQService } from '~libs/common/RabbitMQ';
 import { Logger } from '@nestjs/common';
+import { HttpToRpcExceptionFilter } from '~libs/common/filters/http-to-rpc-exception.filter';
 
 async function bootstrap() {
     const logger = new Logger('order');
     const app = await NestFactory.create(OrderModule);
+
+    app.useGlobalFilters(new HttpToRpcExceptionFilter());
+
     RabbitMQService.connectRabbitMQ({
         app,
         queueNameEnv: 'RABBITMQ_ORDER_QUEUE',
