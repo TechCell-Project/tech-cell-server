360a467f251e89f48aac77394c25dac9c54efdf8
lehuygiang28 <lehuygiang28@gmail.com>
2023-12-27 09:47:23 +0700

    refactor(docker): :recycle: refactor node_exporter

--- containers/node_exporter/node_exporter_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-exec /bin/node_exporter \
-    --path.procfs=/host/proc \
-    --path.sysfs=/host/sys \
-    --path.rootfs=/rootfs \
-    --collector.filesystem.ignored-mount-points=^/(sys|proc|dev|host|etc)($$|/)
