534929aac6732c7fc5ac4da19d666181b438e21e
lehuygiang28 <lehuygiang28@gmail.com>
2023-12-27 08:44:48 +0700

    update: :recycle: update Prometheus configuration and Grafana environment variables

--- containers/prometheus/generate-config.sh
+++ containers/prometheus/generate-config.sh
@@ -2,11 +2,12 @@
 
 cat << EOF > /etc/prometheus/prometheus.yml
 scrape_configs:
-  - job_name: "sws-authtest"
+  - job_name: "api-sws"
     scrape_interval: "10s"
     metrics_path: "/api-stats/metrics"
+    scheme: https
     static_configs:
-      - targets: [api:8000]
+      - targets: [api.techcell.cloud]
     basic_auth:
       username: ${STATS_USER}
       password: ${STATS_PASSWORD}
