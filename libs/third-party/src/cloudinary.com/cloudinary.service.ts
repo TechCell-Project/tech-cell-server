2a67ea9caf3d85d44737a79826a44d2d0f12a2e4
lehuygiang28 <lehuygiang28@gmail.com>
2023-12-29 04:44:23 +0700

    perf(image): :zap: optimize image URLs

--- libs/third-party/src/cloudinary.com/cloudinary.service.ts
+++ libs/third-party/src/cloudinary.com/cloudinary.service.ts
@@ -14,46 +14,39 @@ import { optimizeCloudinaryUrl } from '~libs/common/utils/cloudinary.util';
 @Injectable()
 export class CloudinaryService {
     async uploadImage(file: Express.Multer.File): Promise<CloudinaryResponse> {
-        try {
-            const options: UploadApiOptions = {
-                folder: CLOUDINARY_ROOT_FOLDER_NAME,
-                allowedFormats: CLOUDINARY_ALLOW_IMAGE_FORMATS,
-                public_id: buildPublicId(file),
-                transformation: {
-                    format: 'auto',
-                    quality: 'auto',
-                    fetch_format: 'auto',
-                    dpr: 'auto',
-                },
-            };
+        const options: UploadApiOptions = {
+            folder: CLOUDINARY_ROOT_FOLDER_NAME,
+            allowedFormats: CLOUDINARY_ALLOW_IMAGE_FORMATS,
+            public_id: buildPublicId(file),
+            transformation: {
+                format: 'auto',
+                quality: 'auto',
+                fetch_format: 'auto',
+                dpr: 'auto',
+            },
+        };
 
-            const uploadPromise = (file: Express.Multer.File) =>
-                new Promise<CloudinaryResponse>((resolve, reject) => {
-                    const uploadStream = cloudinary.uploader.upload_stream(
-                        options,
-                        (error, result) => {
-                            if (error) {
-                                return reject(error);
-                            }
-                            resolve(result);
-                        },
-                    );
-
-                    const fileBuffer = Buffer.isBuffer(file.buffer)
-                        ? file.buffer
-                        : Buffer.from(file.buffer);
-                    streamifier.createReadStream(fileBuffer).pipe(uploadStream);
+        const uploadPromise = (file: Express.Multer.File) =>
+            new Promise<CloudinaryResponse>((resolve, reject) => {
+                const uploadStream = cloudinary.uploader.upload_stream(options, (error, result) => {
+                    if (error) {
+                        return reject(error);
+                    }
+                    resolve(result);
                 });
 
-            const response = await uploadPromise(file);
-            return {
-                ...response,
-                url: optimizeCloudinaryUrl(response.url),
-                secure_url: optimizeCloudinaryUrl(response.secure_url),
-            };
-        } catch (error) {
-            throw error;
-        }
+                const fileBuffer = Buffer.isBuffer(file.buffer)
+                    ? file.buffer
+                    : Buffer.from(file.buffer);
+                streamifier.createReadStream(fileBuffer).pipe(uploadStream);
+            });
+
+        const response = await uploadPromise(file);
+        return {
+            ...response,
+            url: optimizeCloudinaryUrl(response.url),
+            secure_url: optimizeCloudinaryUrl(response.secure_url),
+        };
     }
 
     deleteFile(publicId: string): Promise<CloudinaryResponse> {
@@ -68,19 +61,27 @@ export class CloudinaryService {
         });
     }
 
-    getImageByPublicId(publicId: string): Promise<CloudinaryResponse> {
-        return new Promise<CloudinaryResponse>((resolve, reject) => {
-            cloudinary.api.resource(publicId, (error, result) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(result);
-                }
+    async getImageByPublicId(publicId: string): Promise<CloudinaryResponse> {
+        const getImageById = (publicId: string) =>
+            new Promise<CloudinaryResponse>((resolve, reject) => {
+                cloudinary.api.resource(publicId, (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(result);
+                    }
+                });
             });
-        });
+
+        const response = await getImageById(publicId);
+        return {
+            ...response,
+            url: optimizeCloudinaryUrl(response.url),
+            secure_url: optimizeCloudinaryUrl(response.secure_url),
+        };
     }
 
-    getImagesInFolder({
+    public async getImagesInFolder({
         maxResults = 10,
         folderName = CLOUDINARY_ROOT_FOLDER_NAME,
         next_cursor = null,
@@ -103,14 +104,26 @@ export class CloudinaryService {
             },
         };
 
-        return new Promise<ResourceApiResponse>((resolve, reject) => {
-            cloudinary.api.resources(options, (error, result) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(result);
-                }
+        const getImagesInFolder = (options: AdminAndResourceOptions) =>
+            new Promise<ResourceApiResponse>((resolve, reject) => {
+                cloudinary.api.resources(options, (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(result);
+                    }
+                });
             });
+
+        const images = await getImagesInFolder(options);
+        images?.resources?.forEach((image) => {
+            return {
+                ...image,
+                url: optimizeCloudinaryUrl(image?.url),
+                secure_url: optimizeCloudinaryUrl(image?.secure_url),
+            };
         });
+
+        return images;
     }
 }
