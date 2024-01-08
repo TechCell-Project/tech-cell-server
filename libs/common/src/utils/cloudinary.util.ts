ec5bdf2247d801e55fb385ad72858259215c4277
lehuygiang28 <lehuygiang28@gmail.com>
2024-01-08 14:13:03 +0700

    update(libs): :fire: disable cached images with cloudflare [skip sdk]

--- libs/common/src/utils/cloudinary.util.ts
+++ libs/common/src/utils/cloudinary.util.ts
@@ -1,6 +1,8 @@
 export function optimizeCloudinaryUrl(url: string): string {
-    return url.replace(
-        `res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`,
-        `images.techcell.cloud/upload/f_auto,q_auto,dpr_auto`,
-    );
+    // return url.replace(
+    //     `res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`,
+    //     `images.techcell.cloud/upload/f_auto,q_auto,dpr_auto`,
+    // );
+
+    return url;
 }
