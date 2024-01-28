13f7f023f9cd5d4f3f8d4d2d36ad7a9ef66756ba
Lê Huy Giang <lehuygiang28@gmail.com>
2024-01-29 00:40:57 +0700

    feat(metric): add metric with `prom` &  `grafana` (#298)
    * feat(metrics): :rocket: create prometheus
    
    * feat(metrics): :rocket: add Grafana and Prometheus Dockerfiles and configuration files
    
    * update(docker): :recycle: combine run to once
    
    * update(proxy): :sparkles: add nginx proxy configurations for Grafana and Prometheus
    
    * update(docker): :recycle: update Grafana and Prometheus ports
    
    * update: :recycle: change host of targets
    
    * update: :recycle: update Prometheus configuration and Grafana environment variables
    
    * update(grafana): :recycle: update Grafana and Prometheus ports
    
    * update(docker): :rocket: add node_exporter container and related files
    
    * refactor(docker): :recycle: refactor node_exporter
    
    * update(prom): :wrench: update scrape interval for node-exporter
    
    * feat(metrics): :rocket: create prometheus
    
    * feat(metrics): :rocket: add Grafana and Prometheus Dockerfiles and configuration files
    
    * update(docker): :recycle: combine run to once
    
    * update(proxy): :sparkles: add nginx proxy configurations for Grafana and Prometheus
    
    * update(docker): :recycle: update Grafana and Prometheus ports
    
    * update: :recycle: change host of targets
    
    * update: :recycle: update Prometheus configuration and Grafana environment variables
    
    * update(grafana): :recycle: update Grafana and Prometheus ports
    
    * update(docker): :rocket: add node_exporter container and related files
    
    * refactor(docker): :recycle: refactor node_exporter
    
    * update(prom): :wrench: update scrape interval for `node-exporter`
    
    * update(prometheus): :arrow_up: upgrade `prometheus` to stable
    
    [skip ci]

---
+++ containers/prometheus/generate-config.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cat << EOF > /etc/prometheus/prometheus.yml
+scrape_configs:
+  - job_name: "api-sws"
+    scrape_interval: "10s"
+    metrics_path: "/api-stats/metrics"
+    scheme: https
+    static_configs:
+      - targets: [api.techcell.cloud]
+    basic_auth:
+      username: ${STATS_USER}
+      password: ${STATS_PASSWORD}
+
+  - job_name: 'node-exporter'
+    scrape_interval: "10s"
+    static_configs:
+      - targets: ['node_exporter:9092']
+EOF
